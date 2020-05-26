299e00399f27806c7aae61fe0cf46f75acd3ecc0
Renu Jain <renujain2110@gmail.com>
2020-05-26 15:24:15 +0100

    Updates

---
+++ precache-manifest.8eac7df4cbe623becfb45bc43a1d1270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09687b3af900a4fbdb0f9f275549c46",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "8893198b2297189b99b5",
+    "url": "/covidtracker/static/css/main.d706e570.chunk.css"
+  },
+  {
+    "revision": "3521cb71553baaa551fb",
+    "url": "/covidtracker/static/js/2.65cd566e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.65cd566e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8893198b2297189b99b5",
+    "url": "/covidtracker/static/js/main.685c20ba.chunk.js"
+  },
+  {
+    "revision": "3dbbbecccf178defbc3c",
+    "url": "/covidtracker/static/js/runtime-main.2137e695.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/covid19.d7265326.png"
+  }
+]);
